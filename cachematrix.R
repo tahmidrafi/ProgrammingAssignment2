1dc522866c9a2b7628a1760db6b556d4e0a6eaf5
Tahmid Rafi <rafi.tahmid@gmail.com>
2014-07-27 04:46:58 +0600

    Added makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacgeMatrix is a generator of special matrix
+## which stores two matrices:
+##      one is the original matrix
+##      other is the inverse of the matrix
+##
+## It has four functions which can be called:
+##      set(m)      --> input the given matrix and store it
+##      get()       --> return the stored matrix
+##      setinv(inv) --> calculate inverse matrix of the given matrix and store it
+##      getinv()    --> return the stored inverse matrix
 
+## when called it return a list of this four functions
+## By calling each function we can manipulate the stored matrices
+
+makeCacheMatrix <- function(x = matrix()) {
+    
+    ## Create the inverse and set its value to NULL by default
+    inv <- NULL
+    
+    ## Declare set function to re-initialize x
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL ## As x is reset, we have to re-calculate in inv
+    }
+    
+    ## Get returns the matrix itself
+    get <- function() x
+    
+    ## setinv(inv) inputs the inverse function and stores it
+    setinv <- function(inv) inv <<- inv
+    
+    ## getinv() returns the inverse matrix
+    getinv() <- function() inv
+    
 }
 
 
